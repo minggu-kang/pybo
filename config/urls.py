51c9cadedc5a80b4f1ba3bef08a72decd0e6effb
kor1212v <kor1212v@gmailcom>
2021-07-14 09:33:26 +0900

    '추가파일'

--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 from pybo import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pybo/', include('pybo.urls'))
+    path('pybo/', include('pybo.urls')),
+
+
 ]
